5a85a8c7d1abbdb82b3e40a41c1ebde621ef24b0
Eble <markus.eble@sap.com>
2022-11-06 12:03:48 +0100

    Revert "Solution StackMachineParanthese"
    This reverts commit be9ee1a785ba1b9f2bea9d192c1e77cdfd09a17d.

--- StackMachineParanthese.java
+++ StackMachineParanthese.java
@@ -2,37 +2,15 @@
 
 public class StackMachineParanthese extends compiler.StackMachine {
 	
+	@Override
+	public String getStartState() {
+		// expect opening parantheses
+		return "z(";
+	}
 
-
-    @Override
-    public String getStartState() {
-       // expect opening parantheses
-       return "z";
-    }
-
-    @Override
-    public void step() {
-       if ((m_state == "z") && (m_stack.isEmpty()) && (m_input.currentChar() == 0)) {
-         m_state = "z+";
-         return;
-       }
-
-       // if current input is "("
-       if (m_input.currentChar() == '(') {
-         m_stack.push('(');
-         m_input.advance();
-       }  else if (m_input.currentChar() == ')' ) {
-         // if top of stack is "("
-         if (m_stack.lastElement() == '(') {
-           m_stack.pop();
-           m_input.advance();
-         } else {
-          m_state = "z-";
-         }
-       } else {
-         m_state = "z-";
-       }    
-    }
+	@Override
+	public void step() {
+	}
 
 	@Override
 	public boolean isFinalState() {
